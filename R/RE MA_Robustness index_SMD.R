616ee0ab258734ce658419c09b0593245e27537b
LoukiaSpin <61583385+LoukiaSpin@users.noreply.github.com>
2021-01-29 18:51:04 +0100

    Update RE MA_Robustness index_SMD.R

--- R/RE MA_Robustness index_SMD.R
+++ R/RE MA_Robustness index_SMD.R
@@ -13,13 +13,13 @@ lapply(list.of.packages, require, character.only = TRUE); rm(list.of.packages)
 
 
 ## Load functions
-source("R/KLD & Robustness Index.R")     # Estimate the Robustness Index for each scenario and comparison
-source("R/KLD plots_All scenarios.R")    # Panel of KLD.yx plots for all scenarios of one comparison (Makes sense for MA or for one reference-comparison in NMA)
+source("Quantifying-Robustness-in-Meta-analysis/R/KLD & Robustness Index.R")     # Estimate the Robustness Index for each scenario and comparison
+source("Quantifying-Robustness-in-Meta-analysis/R/KLD plots_All scenarios.R")    # Panel of KLD.yx plots for all scenarios of one comparison (Makes sense for MA or for one reference-comparison in NMA)
 
 
 
 ## Load SMD and sd.SMD (Bayesian RE-MA results)
-(SMD <- read.table("data/MA-MOD_SMD.txt", head = T)[, 2:3])
+(SMD <- read.table("Quantifying-Robustness-in-Meta-analysis/data/MA-MOD_SMD.txt", head = T)[, 2:3])
 drug.names <- c("inositol", "placebo")
 nt <- 2
 
