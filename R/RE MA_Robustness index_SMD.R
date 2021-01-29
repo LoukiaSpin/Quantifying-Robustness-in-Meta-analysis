fd6928cf8f957a180b947eea534f76ddcb59f7c5
LoukiaSpin <61583385+LoukiaSpin@users.noreply.github.com>
2021-01-29 18:47:04 +0100

    Update RE MA_Robustness index_SMD.R

--- R/RE MA_Robustness index_SMD.R
+++ R/RE MA_Robustness index_SMD.R
@@ -13,13 +13,13 @@ lapply(list.of.packages, require, character.only = TRUE); rm(list.of.packages)
 
 
 ## Load functions
-source("31_Functions/KLD & Robustness Index.R")     # Estimate the Robustness Index for each scenario and comparison
-source("31_Functions/KLD plots_All scenarios.R")    # Panel of KLD.yx plots for all scenarios of one comparison (Makes sense for MA or for one reference-comparison in NMA)
+source("R/KLD & Robustness Index.R")     # Estimate the Robustness Index for each scenario and comparison
+source("R/KLD plots_All scenarios.R")    # Panel of KLD.yx plots for all scenarios of one comparison (Makes sense for MA or for one reference-comparison in NMA)
 
 
 
 ## Load SMD and sd.SMD (Bayesian RE-MA results)
-(SMD <- read.table("./30_Analysis & Results/MA - Continuous outcome/MA-MOD_SMD.txt", head = T)[, 2:3])
+(SMD <- read.table("data/MA-MOD_SMD.txt", head = T)[, 2:3])
 drug.names <- c("inositol", "placebo")
 nt <- 2
 
@@ -31,10 +31,8 @@ nt <- 2
 
 
 
-## Panel of KLD.yx plots for all scenarios in SMD
-tiff("./30_Analysis & Results/MA - Continuous outcome/KLD Panel.tiff", height = 30, width = 38, units = "cm", compression = "lzw", res = 300)
+## Panel of KLD plots for all scenarios in SMD
 KLD.plots(SMD, primary.scenar = 13, compar = 1, "continuous", drug.names)
-dev.off()
 
 
 
