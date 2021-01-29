94973f2c61efced3a1de3ef9105e0b99d616fe3c
LoukiaSpin <61583385+LoukiaSpin@users.noreply.github.com>
2021-01-29 18:48:14 +0100

    Update RE NMA_Balloon plot_LORs & tau2.R

--- R/RE NMA_Balloon plot_LORs & tau2.R
+++ R/RE NMA_Balloon plot_LORs & tau2.R
@@ -15,14 +15,14 @@ library("ggpubr")
 
 
 ## Load functions
-source("31_Functions/BalloonPlot_Sensitivity analysis_ES.R")
-source("31_Functions/BalloonPlot_Sensitivity analysis_tau2.R")
+source("R/BalloonPlot_Sensitivity analysis_ES.R")
+source("R/BalloonPlot_Sensitivity analysis_tau2.R")
 
 
 
 ## Load database as dataframe (each row is a trial)
-(LOR <- read.table("./30_Analysis & Results/NMA - Binary outcome/NMA-MOD_LOR.txt", head = T)[, 2:5])
-(tau2 <- read.table("./30_Analysis & Results/NMA - Binary outcome/NMA-MOD_tausq.txt", head = T)[, 2:5])
+(LOR <- read.table("data/NMA-MOD_LOR.txt", head = T)[, 2:5])
+(tau2 <- read.table("data/NMA-MOD_tausq.txt", head = T)[, 2:5])
 drug.names <- c("placebo", "budesodine", "budesodine plus formoterol", "fluticasone", 
                 "fluticasone plus salmeterol", "formoterol", "salmeterol", "tiotropium") # PBO is the reference
 drug.names.tau2 <- c("placebo", "active")
@@ -36,17 +36,13 @@ drug.names.tau2 <- c("placebo", "active")
 (p4 <- BalloonPlot.Sensitivity.ES(LOR, compar = 7, "binary", "negative", drug.names))
 
 
-tiff("./30_Analysis & Results/NMA - Binary outcome/BalloonPlot_LOR.tiff", height = 30, width = 33, units = "cm", compression = "lzw", res = 300)
 ggarrange(p1, p4, p3, p2, ncol = 2, nrow = 2, labels = c("A)", "B)", "C)", "D)"))
-dev.off()
 
 
 
 # Balloonplot of tau2
 extent <- summary(rlnorm(1000, -2.06, 1.51))[3] # median
-
-tiff("./30_Analysis & Results/NMA - Binary outcome/BalloonPlot_tau2.tiff", height = 30, width = 33, units = "cm", compression = "lzw", res = 300)
 (p5 <- BalloonPlot.Sensitivity.tau2(tau2, extent, "binary", drug.names.tau2))
-dev.off()
+
 
 
