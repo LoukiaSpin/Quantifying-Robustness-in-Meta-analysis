0904538e6e030d32e1c9c9a57e1af9395b3ef954
LoukiaSpin <61583385+LoukiaSpin@users.noreply.github.com>
2021-01-29 18:45:45 +0100

    Update RE MA_Balloon plot & Barplot KLD_SMD.R

--- R/RE MA_Balloon plot & Barplot KLD_SMD.R
+++ R/RE MA_Balloon plot & Barplot KLD_SMD.R
@@ -10,16 +10,16 @@
 
 
 ## Load functions
-source("31_Functions/BalloonPlot_Sensitivity analysis_ES.R")
-source("31_Functions/BalloonPlot_Sensitivity analysis_tau2.R")
-source("31_Functions/KLD & Robustness Index.R")               # Estimate the Robustness Index for each scenario and comparison
-source("31_Functions/Barplot KLD_All scenarios.R")          
+source("R/BalloonPlot_Sensitivity analysis_ES.R")
+source("R/BalloonPlot_Sensitivity analysis_tau2.R")
+source("R/KLD & Robustness Index.R")               # Estimate the Robustness Index for each scenario and comparison
+source("R/Barplot KLD_All scenarios.R")          
 
 
 
 ## Load necessary estimated model parameters (Bayesian RE-MA results)
-(SMD <- read.table("./30_Analysis & Results/MA - Continuous outcome/MA-MOD_SMD.txt", head = T)[, 2:5])
-(tau2 <- read.table("./30_Analysis & Results/MA - Continuous outcome/MA-MOD_tausq.txt", head = T)[, 2:5])
+(SMD <- read.table("data/MA-MOD_SMD.txt", head = T)[, 2:5])
+(tau2 <- read.table("data/MA-MOD_tausq.txt", head = T)[, 2:5])
 drug.names <- c("placebo", "inositol") 
 
 
@@ -41,17 +41,13 @@ summary(unlist(KLD))
 
 
 ## Bring both plots together
-tiff("./30_Analysis & Results/MA - Continuous outcome/BalloonPlot & Barplot KLD.tiff", height = 20, width = 35, units = "cm", compression = "lzw", res = 300)
 ggarrange(p1, p2, ncol = 2, labels = c("A)", "B)"))
-dev.off()
 
 
 
 # Balloonplot of tau2
 extent <- exp(0.049)   # Rhodes et al. 2015 - PMID: 25304503 (Table 3)
-
-tiff("./30_Analysis & Results/MA - Continuous outcome/BalloonPlot_tau2.tiff", height = 30, width = 33, units = "cm", compression = "lzw", res = 300)
 (p3 <- BalloonPlot.Sensitivity.tau2(tau2, extent, "continuous", drug.names))
-dev.off()
+
 
 
