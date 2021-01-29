dfc99a9f6e23e15b94f818099082a15712987b16
LoukiaSpin <61583385+LoukiaSpin@users.noreply.github.com>
2021-01-29 18:49:06 +0100

    Update RE NMA_Robustness index_LOR.R

--- R/RE NMA_Robustness index_LOR.R
+++ R/RE NMA_Robustness index_LOR.R
@@ -15,14 +15,14 @@ lapply(list.of.packages, require, character.only = TRUE); rm(list.of.packages)
 
 
 ## Load functions
-source("31_Functions/KLD & Robustness Index.R")     # Estimate the Robustness Index for each scenario and comparison
-source("31_Functions/Heatmap D_All comparisons.R")  # Lower triangular heatmap matrix (Makes sense for NMA)
-source("31_Functions/KLD plots_All scenarios.R")    # Panel of KLD.xy plots for all scenarios of one comparison (Makes sense for MA or for one reference-comparison in NMA)
+source("R/KLD & Robustness Index.R")     # Estimate the Robustness Index for each scenario and comparison
+source("R/Heatmap D_All comparisons.R")  # Lower triangular heatmap matrix (Makes sense for NMA)
+source("R/KLD plots_All scenarios.R")    # Panel of KLD.xy plots for all scenarios of one comparison (Makes sense for MA or for one reference-comparison in NMA)
 
 
 
 ## Load database as dataframe (each row is a trial)
-(LOR <- read.table("./30_Analysis & Results/NMA - Binary outcome/NMA-MOD_LOR.txt", head = T)[, 2:3])
+(LOR <- read.table("data/NMA-MOD_LOR.txt", head = T)[, 2:3])
 drug.names <- c("placebo", "budesodine", "budesodine plus formoterol", "fluticasone", 
                 "fluticasone plus salmeterol", "formoterol", "salmeterol", "tiotropium") # PBO is the reference
 nt <- length(drug.names)
@@ -33,16 +33,13 @@ nt <- length(drug.names)
 (RI <- RobustnessIndex(LOR, primary.scenar = 13, nt = nt)$RI)
 (KLD <- RobustnessIndex(LOR, primary.scenar = 13, nt = nt)$kldxy)
 
-tiff("./30_Analysis & Results/NMA - Binary outcome/Heatmap_D.tiff", height = 30, width = 38, units = "cm", compression = "lzw", res = 300)
 HeatMap.AllComparisons.RI(RI, drug.names, 0.28)  
-dev.off()
 
 
 
-## Panel of KLD.yx plots for all scenarios in comparison-specific LOR 
-tiff("./30_Analysis & Results/NMA - Binary outcome/KLD Panel.tiff", height = 20, width = 35, units = "cm", compression = "lzw", res = 300)
+## Panel of KLD plots for all scenarios in comparison-specific LOR 
 KLD.plots(LOR, primary.scenar = 13, compar = 3, "binary", drug.names)
-dev.off()
+
 
 
 
